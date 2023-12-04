a7124159f347c93d17544e7b91461a7929242b63
GrajekF31 <przemekhubacz@interia.pl>
2023-12-04 13:18:15 +0100

    Stworzenie aplikacji

---
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿/*using ChallengeApp2;
+
+User user1 = new User("Adam", "2324fd");
+User user2 = new User("Przemek", "343ffd");
+User user3 = new User("Zuzia", "Porew33");
+User user4 = new User("Kapi", "ewqre");
+
+
+user1.AddScore(1);
+user1.AddScore(4);
+var result = user1.Result;
+Console.WriteLine(result);
+var name = User.GameName;
+var pi = Math.PI;
+Console.WriteLine(name);*/
+using ChallengeApp3;
+Employee employer1 = new Employee(21, "Przemek","Hubacz");
+Employee employer2 = new Employee(13, "Paweł", "Hubacow");
+Employee employer3 = new Employee(29, "Konrad", "Kapusta");
+List<Employee> employers = new List<Employee>()
+{
+    employer1, employer2, employer3
+};
+
+int maxR = -1;
+Employee usemaxR = null;
+employer1.Addpunkty(3);
+employer1.Addpunkty(10);
+employer1.Addpunkty(5);
+employer1.Addpunkty(3);
+employer1.Addpunkty(7);
+
+employer2.Addpunkty(3);
+employer2.Addpunkty(8);
+employer2.Addpunkty(3);
+employer2.Addpunkty(2);
+employer2.Addpunkty(1);
+
+employer3.Addpunkty(3);
+employer3.Addpunkty(1);
+employer3.Addpunkty(1);
+employer3.Addpunkty(5);
+employer3.Addpunkty(8);
+foreach (var Employee in employers)
+{
+    if (Employee.Result > maxR)
+
+    { 
+        maxR = Employee.Result;
+        usemaxR = Employee; }
+}
+Console.WriteLine("Ziomek z największą ilością punktów to " + usemaxR.Name +" " + usemaxR.Surname + " ma " + usemaxR.Age+" lat");
+
