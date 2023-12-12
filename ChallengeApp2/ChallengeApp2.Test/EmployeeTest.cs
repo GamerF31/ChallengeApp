dc2c7b44f1cec9a422bc7a9b8eb1cb9cc3c5be8a
GrajekF31 <przemekhubacz@interia.pl>
2023-12-12 15:34:25 +0100

    testyem

---
+++ ChallengeApp2/ChallengeApp2.Test/EmployeeTest.cs
@@ -0,0 +1,51 @@
+﻿namespace ChallengeApp2.Test
+{
+    public class EmployeeTest
+    {
+        [Test]
+        public void ReturnMaxStat()
+        {
+            //arrange
+            var employee = new Employee1("Przemek", "Hubacz");
+            employee.AddGrade(2);
+            employee.AddGrade(10);
+            employee.AddGrade(1);
+
+            //act
+            var statistics = employee.GetStatistics();
+
+            //assert
+            Assert.AreEqual(10, statistics.Max);
+        }
+        [Test]
+        public void ReturnMinStat() 
+        {
+            //arrange
+            var employee = new Employee1("Przemek", "Hubacz");
+            employee.AddGrade(2);
+            employee.AddGrade(10);
+            employee.AddGrade(1);
+            //act
+            var statistics = employee.GetStatistics();
+            //assert
+            Assert.AreEqual(1, statistics.Min);
+
+
+        }
+        [Test]
+        public void ReturnifAverage()
+        {
+            //arrange
+            var employee = new Employee1("Przemek", "Hubacz");
+            employee.AddGrade(2);
+            employee.AddGrade(10);
+            employee.AddGrade(3);
+            //act
+            var statistics = employee.GetStatistics();
+            //assert
+            Assert.AreEqual(5, statistics.Average);
+
+        }
+
+    }
+}
