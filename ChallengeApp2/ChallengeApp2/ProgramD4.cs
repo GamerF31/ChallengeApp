f4b25965e0e638127f44a3faf558f84616fd1959
GrajekF31 <przemekhubacz@interia.pl>
2023-12-12 14:16:23 +0100

    d4

---
+++ ChallengeApp2/ChallengeApp2/ProgramD4.cs
@@ -0,0 +1,104 @@
+﻿// liczby całkowite
+/*
+int myAge = 120;
+int newAge = myAge + 5;
+//Console.WriteLine(newAge);
+int myVar = int.MinValue;
+uint myVar2 = uint.MaxValue;
+long myVar3 = long.MaxValue;
+// liczby zmiennoprzecinkowe
+float myNumber = float.MaxValue;
+double myNumber2 = double.MaxValue;
+decimal myNumber3 = decimal.MaxValue;
+
+// zmienne tekstowe
+string name = "Przemek";
+string surname = "Hubacz";
+string result = name + " " + surname + " " + myAge;
+//Console.WriteLine(result);
+
+char myVar5 = 'c';
+var result2 = name.ToArray();
+Console.WriteLine(result2);
+// zmienna logiczna
+bool isActive = true;
+isActive = false;
+var isValid = 5 > 6;
+Console.WriteLine(isValid);
+
+var number1 = 5;
+var number2 = 10;
+var namee = "Adam";
+bool asis = true;
+var agee = 340;
+if (asis)
+{
+    Console.WriteLine("Jestem adame przed 50");
+}
+else
+{
+    Console.WriteLine("Jestem nikim");
+}
+
+if (agee > 0 && agee < 30)
+{
+    Console.WriteLine("Are you never going home");
+}
+else if (agee < 0)
+{
+    Console.WriteLine("Let's go tek you know");
+}
+else
+{
+    Console.WriteLine("Whoo whooaa");
+}
+if (agee > 50)
+{
+    if (namee == "Adam")
+    {
+        Console.WriteLine("Siemano jestem borys");
+    }
+    else
+    {
+        Console.WriteLine("Siemano nie jestem borys");
+    }
+
+}
+else
+{
+    Console.WriteLine("Siema niewiem kima ja kekw");
+}
+if (agee > 50) { }
+*/
+/*
+string name = "Przemek";
+var płeć = "mężczyzna";
+int age = 210;
+Console.WriteLine(name + ", lat" + age);
+if (płeć == "mężczyzna")
+{
+    if (age > 30)
+    {
+        Console.WriteLine("Mężczyzna powyzej 30 lat");
+    }
+    else
+    {
+        if (age < 18)
+        {
+            Console.WriteLine("Niepełnoletni mężczyzna");
+
+        }
+        Console.WriteLine("Mężczyzna poniżej 30 lat");
+
+    }
+
+}
+else if (płeć == "kobieta")
+{
+    Console.WriteLine("Jesteś kobietą");
+}
+else
+{
+    Console.WriteLine("Ciul wie kto ty");
+}
+*/
